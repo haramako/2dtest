cdc2bdf8bff7ca041be25446bb5a9b5fc9a3b0a8
HARADA Makoto <haramako@gmail.com>
2017-03-03 19:45:52 +0900

    change

---
+++ Assets/ActiveOnTrigger.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+public class ActiveOnTrigger : MonoBehaviour {
+
+    public void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            GetComponent<Animator>().SetBool("Active", true);
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            GetComponent<Animator>().SetBool("Active", false);
+        }
+    }
+}
